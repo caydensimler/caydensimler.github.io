58bc74f4c5d543fbf14c2fa22b3b66d91e7b00a4
Cayden <cayden.simler@hotmail.com>
2017-03-09 14:51:32 -0600

    Wired up the buttons the resume page to display each sub-header.

--- js/personal-site.js
+++ js/personal-site.js
@@ -7,6 +7,9 @@ $("document").ready(function(){
 		$(".languages").hide();
 		$(".contact").hide();
 		$(".faq").hide();
+		$(".skills").hide();
+		$(".experience").hide();
+		$(".education").hide();
 	}
 
 	hideAll()
@@ -41,33 +44,60 @@ $("document").ready(function(){
 		});
 	});
 
+	$(".objectiveButton").click(function(){
+		$(".objective").fadeToggle(1200);
+		$(".skills").hide();
+		$(".experience").hide();
+		$(".education").hide();
+	});
+
+	$(".skillsButton").click(function(){
+		$(".skills").fadeToggle(1200);
+		$(".objective").hide();
+		$(".experience").hide();
+		$(".education").hide();
+	});
+
+	$(".educationButton").click(function(){
+		$(".education").fadeToggle(1200);
+		$(".objective").hide();
+		$(".experience").hide();
+		$(".skills").hide();
+	});
+
+	$(".experienceButton").click(function(){
+		$(".experience").fadeToggle(1200);
+		$(".objective").hide();
+		$(".education").hide();
+		$(".skills").hide();
+	})
+
 	$(".languagesButton").click(function(){
 		$(".languages").fadeToggle(800);
 		$(".aboutMe").hide();
 		$(".contact").hide();
 		$(".faq").hide();
-	})
+	});
 
 	$(".aboutMeButton").click(function(){
 		$(".aboutMe").fadeToggle(800);
 		$(".contact").hide();
 		$(".faq").hide();
 		$(".languages").hide();
-
-	})
+	});
 
 	$(".contactButton").click(function(){
 		$(".contact").fadeToggle(800);
 		$(".aboutMe").hide();
 		$(".languages").hide();
 		$(".faq").hide();
-	})
+	});
 
 	$(".faqButton").click(function(){
 		$(".faq").fadeToggle(800);
 		$(".languages").hide();
 		$(".aboutMe").hide();
 		$(".contact").hide();
-	})
+	});
 
 });
