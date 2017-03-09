5c43bc03275530e49e3bf537af84d135fa879dd4
Cayden <cayden.simler@hotmail.com>
2017-03-09 10:40:10 -0600

    Created a new layout for my homepage to include buttons that reveal information including an About Me, Languages, Contact, and FAQ sections.

---
+++ js/personal-site.js
@@ -0,0 +1,46 @@
+"use strict";
+
+$("document").ready(function(){
+
+
+	function hideAll(){
+		$(".languages").hide();
+		$(".aboutMe").hide();
+		$(".contact").hide();
+		$(".faq").hide();
+	}
+
+	hideAll()
+
+	$(".homepageButton").hover(function(){
+		$(this).css({
+			"color": "#383838",
+			"backgroundColor": "#F5F1ED",
+			"fontWeight": "bold"
+		});
+
+		}, function() {
+		$(this).css({
+			"color": "#F5F1ED",
+			"backgroundColor": "#383838",
+			"fontWeight": "normal"
+		});
+	});
+
+	$(".languagesButton").click(function(){
+		$(".languages").fadeToggle(800);
+	})
+
+	$(".aboutMeButton").click(function(){
+		$(".aboutMe").fadeToggle(800);
+	})
+
+	$(".contactButton").click(function(){
+		$(".contact").fadeToggle(800);
+	})
+
+	$(".faqButton").click(function(){
+		$(".faq").fadeToggle(800);
+	})
+
+});
